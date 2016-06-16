5ca5a2d44708b3aae069bf66c7c554cb5bfdb86a
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-06-16 16:58:11 -0300

    validação incluir

--- server/routes/incluir.js
+++ server/routes/incluir.js
@@ -16,8 +16,12 @@ var errors = {
 
 incluir.post('/', function(req, res) {
     if(req.query.hasOwnProperty('Sala') && req.query.hasOwnProperty('Inicio') && req.query.hasOwnProperty('Termino') && req.query.hasOwnProperty('Data')){
+<<<<<<< 2cc7f389967be24a7b1140ee05d852ef2660eb47
 <<<<<<< 9dc861cab1b6c68f9b89bba88d861a1fdb0aa179
 <<<<<<< fdfb595b73e6e0f3b35c03e4ce15ab45bd718d4b
+=======
+<<<<<<< b10bea667334b0488f9129a2d87609396e9dccbe
+>>>>>>> validação incluir
         var inicio = req.query.Inicio.replace(/[:-]/g, '');
         var termino = req.query.Termino.replace(/[:-]/g, '');
         var data = req.query.Data.replace(/[:-]/g, '');
@@ -64,6 +68,7 @@ incluir.post('/', function(req, res) {
         var inicio = req.query.Inicio.replace(/[:-.,]/g, '');
         var termino = req.query.Termino.replace(/[:-.,]/g, '');
         var data = req.query.Data.replace(/[:-.,]/g, '');
+<<<<<<< 2cc7f389967be24a7b1140ee05d852ef2660eb47
 =======
         var inicio = req.query.Inicio.replace(/[:-]/g, '');
         var termino = req.query.Termino.replace(/[:-]/g, '');
@@ -102,6 +107,29 @@ incluir.post('/', function(req, res) {
                     res.status(403).send(errors.emptyField);
                 }
 <<<<<<< 9dc861cab1b6c68f9b89bba88d861a1fdb0aa179
+=======
+        var flagError = false;
+        connection.query('SELECT * FROM reservas WHERE Sala = ? AND Data = ?', [parseInt(req.query.Sala, 10), parseInt(data, 10)], function(err, result){
+            if (err) throw err;
+            for(var x=0;x<result.length;x++){
+                var inicioReuniao = result[x].Inicio.replace(/[:-]/g, '');
+                var terminoReuniao = result[x].Termino.replace(/[:-]/g, '');
+                if(inicio>=inicioReuniao && inicio<terminoReuniao){
+                    flagError = true;
+                    x=result.length+1;
+                    res.sendStatus(403);
+                }
+                else if(termino>inicioReuniao && termino <=terminoReuniao){
+                    flagError = true;
+                    x=result.length+1;
+                    res.sendStatus(403);
+                }
+                else if(inicio<=inicioReuniao && termino>=terminoReuniao){
+                    flagError = true;
+                    x=result.length+1;
+                    res.sendStatus(403);
+                }
+>>>>>>> validação incluir
             }
             if(!flagError){
                 connection.query('INSERT INTO reservas SET ?', req.query, function(err, result){
@@ -112,6 +140,7 @@ incluir.post('/', function(req, res) {
             }
         });
 >>>>>>> validação incluir
+<<<<<<< 2cc7f389967be24a7b1140ee05d852ef2660eb47
     }
     else {
         console.log('Failed to insert, insuficient query');
@@ -176,6 +205,8 @@ incluir.put('/', function(req, res) {
             res.type('text/json');
             res.status(403).send(errors.emptyField);
         }
+=======
+>>>>>>> validação incluir
     }
     else {
         console.log('Failed to insert, insuficient query');

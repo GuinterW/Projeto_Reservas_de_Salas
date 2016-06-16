7f707896c4742ada71637d20850817385e4198a0
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-06-16 16:58:11 -0300

    validação incluir

--- server/routes/incluir.js
+++ server/routes/incluir.js
@@ -16,12 +16,39 @@ var errors = {
 
 incluir.post('/', function(req, res) {
     if(req.query.hasOwnProperty('Sala') && req.query.hasOwnProperty('Inicio') && req.query.hasOwnProperty('Termino') && req.query.hasOwnProperty('Data')){
-        connection.query('INSERT INTO reservas SET ?', req.query, function(err, result){
+        var inicio = req.query.Inicio.replace(/[:-.,]/g, '');
+        var termino = req.query.Termino.replace(/[:-.,]/g, '');
+        var data = req.query.Data.replace(/[:-.,]/g, '');
+        var flagError = false;
+        connection.query('SELECT * FROM reservas WHERE Sala = ? AND Data = ?', [parseInt(req.query.Sala, 10), parseInt(data, 10)], function(err, result){
             if (err) throw err;
-            console.log('Inserted, ID=' + result.insertId);
+            for(var x=0;x<result.length;x++){
+                var inicioReuniao = result[x].Inicio.replace(/[:-]/g, '');
+                var terminoReuniao = result[x].Termino.replace(/[:-]/g, '');
+                if(inicio>=inicioReuniao && inicio<terminoReuniao){
+                    flagError = true;
+                    x=result.length+1;
+                    res.sendStatus(403);
+                }
+                else if(termino>inicioReuniao && termino <=terminoReuniao){
+                    flagError = true;
+                    x=result.length+1;
+                    res.sendStatus(403);
+                }
+                else if(inicio<=inicioReuniao && termino>=terminoReuniao){
+                    flagError = true;
+                    x=result.length+1;
+                    res.sendStatus(403);
+                }
+            }
+            if(!flagError){
+                connection.query('INSERT INTO reservas SET ?', req.query, function(err, result){
+                    if (err) throw err;
+                    console.log('Inserted, ID=' + result.insertId);
+                });
+                res.sendStatus(200);
+            }
         });
-        connection.end();
-        res.sendStatus(200);
     }
     else {
         console.log('Failed to insert');

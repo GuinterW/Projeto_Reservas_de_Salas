bc13c95da103079c13e02172c7e52df15b0e818c
Guinter Weber <guinter.weber@digitaldesk.com.br>
2016-06-17 14:11:34 -0300

    arquivo consulta, .get

--- server/routes/consulta.js
+++ server/routes/consulta.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var consulta = express.Router();
 
-consulta.get('/:sala', function(req, res) {
-	console.log(req.params.sala);
+//CONEXAO SERVER.
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : '192.168.1.117',
+  user     : 'me',
+  password : 'secret',
+  database : 'reservas'
+});
+
+consulta.get('/:sala/:ano', function(req, res){
+	console.log(req.params);
+	res.sendStatus(200);
+});
+
+consulta.get('/:sala/:ano/:mes', function(req, res){
+	console.log(req.params);
+	res.sendStatus(200);
+});
+
+consulta.get('/:sala/:ano/:mes/:dia', function(req, res) {
+	console.log(req.params);
 	res.sendStatus(200);
 });
 

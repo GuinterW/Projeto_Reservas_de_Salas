5522187ba47442845662ac4e3d9f3805c80c06d3
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-06-17 16:55:51 -0300

    validação id

--- server/routes/incluir.js
+++ server/routes/incluir.js
@@ -42,9 +42,15 @@ incluir.post('/', function(req, res) {
                 if(!flagError){
                     connection.query('INSERT INTO reservas SET ?', req.query, function(err, result){
                         if (err) throw err;
-                        console.log('Inserted, ID=' + result.insertId);
+                        if(result.affectedRows>0){
+                            console.log('Inserted, ID=' + result.insertId);
+                            res.sendStatus(200);
+                        }
+                        else {
+                            console.log('Failed to insert, invalid ID');
+                            res.status(403).send(errors.emptyField);
+                        }
                     });
-                    res.sendStatus(200);
                 }
                 else {
                     console.log('Failed to insert, already exist reserve');
@@ -71,16 +77,6 @@ incluir.put('/', function(req, res) {
         var inicio = req.query.Inicio.replace(/[:-]/g, '');
         var termino = req.query.Termino.replace(/[:-]/g, '');
         var data = req.query.Data.replace(/[:-]/g, '');
-
-        /*var id = parseInt(req.query.ID, 10);
-        console.log(typeof(id));
-        console.log(id);
-        if(id!=NaN){
-            console.log('Failed to insert, invalid ID');
-            res.type('text/json');
-            res.status(403).send(errors.emptyField);
-        }*/
-
         var flagError = false;
         if(inicio.length>=5 && termino.length>=5 && data.length==8 && req.query.Sala.length==1 && parseInt(req.query.Inicio, 10)!='NaN' && parseInt(req.query.Termino, 10)!='NaN' && parseInt(req.query.Data, 10)!='NaN' && parseInt(req.query.Sala, 10)!='NaN'){
             connection.query('SELECT * FROM reservas WHERE Sala = ? AND Data = ?', [parseInt(req.query.Sala, 10), parseInt(req.query.Data, 10)], function(err, result){
@@ -104,9 +100,15 @@ incluir.put('/', function(req, res) {
                 if(!flagError){
                     connection.query('UPDATE reservas SET ? WHERE ID = ?', [req.query, req.query.ID], function(err, result){
                         if (err) throw err;
-                        console.log('Updated, ID=' + req.query.ID);
+                        if(result.affectedRows>0){
+                            console.log('Updated, ID=' + result.insertId);
+                            res.sendStatus(200);
+                        }
+                        else {
+                            console.log('Failed to insert, invalid ID');
+                            res.status(403).send(errors.emptyField);
+                        }
                     });
-                    res.sendStatus(200);
                 }
                 else {
                     console.log('Failed to insert, already exist reserve');

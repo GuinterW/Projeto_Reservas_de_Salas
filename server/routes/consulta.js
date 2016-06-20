f713337efb232c48aaef4678e4881e902ee9b14b
Guinter Weber <guinter.weber@digitaldesk.com.br>
2016-06-20 14:10:06 -0300

    conexao das consultas com o database para pegar itens especificos

--- server/routes/consulta.js
+++ server/routes/consulta.js
@@ -10,19 +10,29 @@ var connection = mysql.createConnection({
   database : 'reservas'
 });
 
-consulta.get('/:sala/:ano', function(req, res){
+consulta.get('/:Sala/:Ano', function(req, res){
 	console.log(req.params);
+    connection.query('SELECT * FROM reservas WHERE Sala = ? AND YEAR(Data) = ?', [parseInt(req.params.Sala, 10), parseInt(req.params.Ano, 10)], function(err, result){
+	console.log(result);
 	res.sendStatus(200);
+	});
 });
 
-consulta.get('/:sala/:ano/:mes', function(req, res){
+consulta.get('/:Sala/:Ano/:Mes', function(req, res){
 	console.log(req.params);
-	res.sendStatus(200);
+    connection.query('SELECT * FROM reservas WHERE Sala = ? AND YEAR(Data) = ? AND MONTH(Data) = ?', [parseInt(req.params.Sala, 10), parseInt(req.params.Ano, 10), parseInt(req.params.Mes)], function(err, result){
+		console.log(result);
+		res.sendStatus(200);
+	});
 });
 
-consulta.get('/:sala/:ano/:mes/:dia', function(req, res) {
+consulta.get('/:Sala/:Ano/:Mes/:Dia', function(req, res) {
 	console.log(req.params);
+	var Data= req.params.Ano + req.params.Mes + req.params.Dia;   
+    connection.query('SELECT * FROM reservas WHERE Sala = ? AND Data = ?', [parseInt(req.params.Sala, 10), parseInt(Data, 10)], function(err, result){
+	console.log(result);
 	res.sendStatus(200);
+	});
 });
 
 module.exports = consulta;

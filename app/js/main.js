ec959aa0eb526d7b5d057cc3f3752bce67d0a6aa
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-06-20 14:20:40 -0300

    paginação e layout

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,18 @@
+var ip;
+
 $(document).ready(function(){
+	start();
 	$('.links').click(function(){
 		var pagina = $(this).attr('href');
-		console.log(pagina);
+		$("li[class='active oi']").removeClass("active oi");
+		$("li a[href='"+pagina+"']").parent().addClass("active oi");
+    	//$("li .links[href="+pagina+"]").parent().addClass("active");
 		$('.pages').hide();
 		$(pagina).show();
 	});
 });
+
+function start (){
+	$('.pages').hide();
+	$('#forToday').show();
+}

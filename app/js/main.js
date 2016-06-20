709c01f8d933912fdc07cc1097795df4c8043720
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-06-20 16:50:17 -0300

    Identificacao links

--- app/js/main.js
+++ app/js/main.js
@@ -16,7 +16,14 @@ var table = {
 }
 =======
 var ip;
+<<<<<<< 14a1f27177e15aabde17da4ec20b8b85e45731ca
 >>>>>>> paginação e layout
+=======
+var table = {
+	forToday: '<table class="table table-bordered table-striped"><thead><tr><th>Início</th><th>Término</th><th>Responsável</th><th>Pauta</th></tr></thead><tbody class="newLine"></tbody></table>',
+	complete: '<table class="table table-bordered table-striped"><thead><tr><th>Data</th><th>Início</th><th>Término</th><th>Responsável</th><th>Pauta</th></tr></thead><tbody class="newLine"></tbody></table>'
+}
+>>>>>>> Identificacao links
 
 $(document).ready(function(){
 	start();
@@ -32,9 +39,8 @@ $(document).ready(function(){
 		formTable (1);
 =======
 		var pagina = $(this).attr('href');
-		$("li[class='active oi']").removeClass("active oi");
-		$("li a[href='"+pagina+"']").parent().addClass("active oi");
-    	//$("li .links[href="+pagina+"]").parent().addClass("active");
+		$("li[class='active mainLinks']").removeClass("active mainLinks");
+		$("li a[href='"+pagina+"']").parent().addClass("active mainLinks");
 		$('.pages').hide();
 		$(pagina).show();
 >>>>>>> pages
@@ -48,6 +54,7 @@ $(document).ready(function(){
 	$('#repeat').click(function(){
 		$("#myModal").modal();
 	});
+<<<<<<< 14a1f27177e15aabde17da4ec20b8b85e45731ca
 <<<<<<< 90b9c99e06b977b01c56077a7de16fc13feb00a2
 	$('#calendar').datepicker({
         format: "dd/mm/yyyy",
@@ -55,11 +62,21 @@ $(document).ready(function(){
     });
 =======
 >>>>>>> paginação e layout
+=======
+	$('.room').click(function(){
+		var pagina = $(this).attr('id');
+		console.log(pagina);
+		$("li[class='active activeRoom']").removeClass("active activeRoom");
+		$("li a[id='"+pagina+"']").parent().addClass("active activeRoom");
+		formTable ();
+	});
+>>>>>>> Identificacao links
 });
 
 function start (){
 	$('.pages').hide();
 	$('#forToday').show();
+<<<<<<< 14a1f27177e15aabde17da4ec20b8b85e45731ca
 <<<<<<< 90b9c99e06b977b01c56077a7de16fc13feb00a2
 	tableForToday (1);
 }
@@ -100,12 +117,26 @@ function tableForToday (sala){
 	$("#pagesAndTable").show();
 	var result='';
 	var list = table.forToday;
+=======
+}
+
+function formTable (){
+	var pagina = $("li[class='active mainLinks'] a").attr('href');
+	console.log(pagina);
+	/*if (pagina=='#forToday'){
+		console.log('hojeeeee');
+	}
+	else console.log ('outrooo');
+	var result='';
+	$('.newLine').html('');
+>>>>>>> Identificacao links
 	for (x=0;x<10;x++){
 		result += '<tr><td>hh:mm</td>';
 		result += '<td>hh:mm</td>';
 		result += '<td>Pessoa</td>';
 		result += '<td>Assunto</td></tr>';
 	}
+<<<<<<< 14a1f27177e15aabde17da4ec20b8b85e45731ca
 	$('#table').html(list);
 	$('.newLine').append(result);
 }
@@ -141,3 +172,7 @@ function dateToday (){
 =======
 >>>>>>> paginação e layout
 }
+=======
+	$('.newLine').append(result);*/
+}
+>>>>>>> Identificacao links

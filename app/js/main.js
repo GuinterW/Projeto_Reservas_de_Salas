8e38f336a921f53a324db22afe82aa73dde4f696
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-06-22 16:21:43 -0300

    logica salas

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
-var ip;
+var ip = {
+	query: 'http://localhost:9000/search/'
+}
+
+//só pra eu saber
+/*
+date: 'http://localhost:9000/search/sala/ano/mes/dia',
+yearAndMonth: 'http://localhost:9000/search/sala/ano/mes',
+year: 'http://localhost:9000/search/sala/ano'
+*/
+
 var table = {
 	forToday: '<table class="table table-bordered table-striped"><thead><tr><th>Início</th><th>Término</th><th>Responsável</th><th>Pauta</th></tr></thead><tbody class="newLine"></tbody></table>',
 	complete: '<table class="table table-bordered table-striped"><thead><tr><th>Data</th><th>Início</th><th>Término</th><th>Responsável</th><th>Pauta</th></tr></thead><tbody class="newLine"></tbody></table>'
@@ -18,7 +28,7 @@ $(document).ready(function(){
 		var pagina = $(this).attr('id');
 		$("li[class='active activeRoom']").removeClass("active activeRoom");
 		$("li a[id='"+pagina+"']").parent().addClass("active activeRoom");
-		formTable ();
+		changingRoom (pagina);
 	});
 	$('#repeat').click(function(){
 		console.log('checkeddd');
@@ -41,7 +51,20 @@ function cleanTable (){
 	$('.newLine').html('');
 }
 
+function changingRoom (pagina){
+	if (pagina=='room1') {
+		console.log('ummmmmm');
+	}
+	else if (pagina=='room2') {
+		console.log('doiss');
+	}
+	else {
+		console.log('outraa');
+	}
+}
+
 function formTable (){
+	$("li[class='active activeRoom']").removeClass("active activeRoom");
 	var pagina = $("li[class='active mainLinks'] a").attr('href');
 	if (pagina=='#forToday'){
 		tableForToday ();
@@ -55,6 +78,8 @@ function formTable (){
 }
 
 function tableForToday (){
+	$("li[class='active activeRoom']").removeClass("active activeRoom");
+	$("li a[id='room1']").parent().addClass("active activeRoom");
 	cleanTable ();
 	dateToday ();
 	$("#pagesAndTable").show();
@@ -71,6 +96,7 @@ function tableForToday (){
 }
 
 function tableComplete (){
+	$("li a[id='room1']").parent().addClass("active activeRoom");
 	cleanTable ();
 	$("#pagesAndTable").show();
 	var result='';
@@ -93,9 +119,12 @@ function formInclusion (){
 
 function dateToday (){
 	var data = new Date(),
-        dia  = data.getDate(),
-        mes  = data.getMonth() + 1,
-        ano  = data.getFullYear();
-    var dateComplete= [dia, mes, ano].join('/');
-   	$('#forToday').html(dateComplete);
+        day  = data.getDate(),
+        month  = data.getMonth() + 1,
+        year  = data.getFullYear();
+    console.log(day);
+    console.log(month);
+    console.log(year);
+    var dateComplete= '<i class="fa fa-calendar" aria-hidden="true"></i>' + [day, month, year].join('/');
+   	$('#dateToday').html(dateComplete);
 }

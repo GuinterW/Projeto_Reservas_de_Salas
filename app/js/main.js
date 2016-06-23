7967d249936559eccc0a591bfb49fe750de50042
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-06-23 13:09:41 -0300

    alteracoes depois dos conflitos

---
+++ app/js/main.js
@@ -0,0 +1,141 @@
+var ip = {
+	query: 'http://localhost:9000/search/'
+}
+
+//só pra eu saber
+/*
+date: 'http://localhost:9000/search/sala/ano/mes/dia',
+yearAndMonth: 'http://localhost:9000/search/sala/ano/mes',
+year: 'http://localhost:9000/search/sala/ano'
+*/
+
+var table = {
+	forToday: '<table class="table table-bordered table-striped"><thead><tr><th>Início</th><th>Término</th><th>Responsável</th><th>Pauta</th></tr></thead><tbody class="newLine"></tbody></table>',
+	complete: '<table class="table table-bordered table-striped"><thead><tr><th>Data</th><th>Início</th><th>Término</th><th>Responsável</th><th>Pauta</th></tr></thead><tbody class="newLine"></tbody></table>'
+}
+
+$(document).ready(function(){
+	start();
+	$('.links').click(function(){
+		$("li[class='active activeRoom']").removeClass("active activeRoom");
+		$("li a[id='room1']").parent().addClass("active activeRoom");
+		var pagina = $(this).attr('href');
+		$("li[class='active mainLinks']").removeClass("active mainLinks");
+		$("li a[href='"+pagina+"']").parent().addClass("active mainLinks");
+		$('.pages').hide();
+		$(pagina).show();
+		formTable (1);
+	});
+	$('.room').click(function(){
+		var pagina = $(this).attr('id');
+		$("li[class='active activeRoom']").removeClass("active activeRoom");
+		$("li a[id='"+pagina+"']").parent().addClass("active activeRoom");
+		changingRoom (pagina);
+	});
+	$('#repeat').click(function(){
+		$("#myModal").modal();
+	});
+	$('#calendar').datepicker({
+        format: "dd/mm/yyyy",
+        language: "pt-br"
+    });
+});
+
+function start (){
+	$('.pages').hide();
+	$('#forToday').show();
+	tableForToday (1);
+}
+
+function cleanTable (){
+	$('#table').html('');
+	$('.newLine').html('');
+}
+
+function changingRoom (pagina){
+	if (pagina=='room1') {
+		formTable(1);
+	}
+	else if (pagina=='room2') {
+		formTable(2);
+	}
+	else {
+		formTable(3);
+	}
+}
+
+function formTable (sala){
+	var pagina = $("li[class='active mainLinks'] a").attr('href');
+	if (pagina=='#forToday'){
+		tableForToday (sala);
+	}
+	else if (pagina=='#listMeetings'){
+		tableComplete (sala);
+	}
+	else {
+		formInclusion ();
+	}
+}
+
+function tableForToday (sala){
+	cleanTable ();
+	dateToday ();
+	$("#pagesAndTable").show();
+	var result='';
+	var list = table.forToday;
+}
+
+function changingRoom (pagina){
+	if (pagina=='room1') {
+		formTable(1);
+	}
+	else if (pagina=='room2') {
+		formTable(2);
+	}
+	else {
+		formTable(3);
+	}
+}
+
+function formTable (sala){
+	var pagina = $("li[class='active mainLinks'] a").attr('href');
+	if (pagina=='#forToday'){
+		tableForToday (sala);
+	}
+	else if (pagina=='#listMeetings'){
+		tableComplete (sala);
+	}
+	else {
+		formInclusion ();
+	}
+}
+
+function tableForToday (sala){
+	cleanTable ();
+	dateToday ();
+	$("#pagesAndTable").show();
+	var result='';
+	var list = table.forToday;
+	for (x=0;x<10;x++){
+		result += '<tr><td>hh:mm</td>';
+		result += '<td>hh:mm</td>';
+		result += '<td>Pessoa</td>';
+		result += '<td>Assunto</td></tr>';
+	}
+	$('#table').html(list);
+	$('.newLine').append(result);
+}
+
+function formInclusion (){
+	$("#pagesAndTable").hide();
+	cleanTable ();
+}
+
+function dateToday (){
+	var data = new Date(),
+        day  = data.getDate(),
+        month  = data.getMonth() + 1,
+        year  = data.getFullYear();
+    var dateComplete= '<i class="fa fa-calendar" aria-hidden="true"></i>' + [day, month, year].join('/');
+   	$('#dateToday').html(dateComplete);
+}
